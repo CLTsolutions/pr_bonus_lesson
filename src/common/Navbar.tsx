c112a14de5bbbebf12c07591d54fcfdfaa240175
Justin Ahmann <jahmanndev@gmail.com>
2021-09-18 14:54:07 -0400

    added naviagtion

---
+++ src/common/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+export class Navbar extends Component {
+    render(){
+        return (
+            <nav>
+                This is a nav bar
+            </nav>
+        )
+    }
+}
